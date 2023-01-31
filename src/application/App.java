aca7b3eebfa8d732df275f447b1231c0d9eadfe1
zSoony <suenerik@hotmail.com>
2023-01-31 03:12:56 -0300

    avançando na aula

--- src/application/App.java
+++ src/application/App.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 
 public class App {
    
+ 
     public static void main(String[] args) {
 
        Scanner sc = new Scanner (System.in);
       System.out.println("primeira alteração");
+
+      
       
        
     
